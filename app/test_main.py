f12ef1ed89b7be1f5c5236211414fdbac460a1b8
Yevgeniy Marchenko <Jackson.stethem@gmail.com>
2026-04-23 22:02:23 +0300

    Implement tests for function "check_password"

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,19 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, expected_action",
+    [
+        ("Str@ng1", False),
+        ("qwerty", False),
+        ("Cucaracha", False),
+        ("b@mbino1", False),
+        ("Pipi1@pupuchecking", False),
+        ("P@ssword1", True)
+    ]
+)
+def test_check_password(password: str, expected_action: bool) -> None:
+    result = check_password(password)
+
+    assert result == expected_action
